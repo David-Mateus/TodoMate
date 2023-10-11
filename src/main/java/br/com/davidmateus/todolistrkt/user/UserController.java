c68734ca01ace4eed9d17c4163a5bcd199220d6b
David-Mateus <david-mateus09@hotmail.com>
2023-10-11 14:44:30 -0300

    integracao com BD

--- src/main/java/br/com/davidmateus/todolistrkt/user/UserController.java
+++ src/main/java/br/com/davidmateus/todolistrkt/user/UserController.java
@@ -1,13 +1,30 @@
 package br.com.davidmateus.todolistrkt.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
 
+    @GetMapping("/")
+    public List<UserModel> getAllUsers(){
+        return  this.userRepository.findAll();
+    }
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            System.out.println("Já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
